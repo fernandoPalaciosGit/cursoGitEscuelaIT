ca61e0f6d8c72da181474a98f00c58fec16f6dce
Fernando Palacios Landi <fernandopalacioslandi@gmail.com>
2014-06-07 13:08:51 +0200

    validacion de nombre de cliente

--- validacion.js
+++ validacion.js
@@ -1,5 +1,18 @@
+//validacion de formulario en cliente
+var inti = function(e_load){
+	document.querySelector('[type:"submit"]').addEvenListener('click', validaciones, false);
+	var attrCliente = {
+		name: 'Pepito',
+		surname: 'de los Palotes'
+	};
+	var cliente = new Cliente(attrCliente);
+	if ( cliente.getName() !== '' )
+		document.documentElement.lastChild.innerHTML += '<strong>'.cliente.getName().'</strong>';
+;
+
 //prototypo de cliente
 var Cliente = function(attrClient){
+	var attrClient = attrClient || {};
 	var API = {};
 	var name = attrClient.name;
 	var surname = attrClient.surname;
@@ -9,3 +22,10 @@ var Cliente = function(attrClient){
 	
 	return API;
 }
+
+var validaciones = function(e_click){
+	e.click.preventDefault();
+	console.log('validaciones de cliente antes de enviar el formulario');
+};
+
+window.onload = init;
