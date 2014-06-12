ecb3fd0fe6b73ec1989050b8b61be47aa86ca775
Fernando Palacios Landi <fernandopalacioslandi@gmail.com>
2014-06-12 10:01:13 +0200

    splash: ajax datos de cliente + conexion Mysql

--- validacion.js
+++ validacion.js
@@ -1,16 +1,60 @@
-<<<<<<< HEAD
+var	AJAX = {
+	xhr: null,
+	cuentaAtras: null,
+	timeoutAjax : 4000, //limite de tiempo de espera de petición
+	shim: function(){
+		if( window.XMLHttpRequest )
+			this.xhr = new XMLHttpRequest();
+		else if( window.ActiveXObject )
+			this.xhr = new ActiveXObject("Microsoft.XMLHTTP");
+		else
+			this.xhr = null;
+		return this.xhr;
+	},
+	state: {
+		READY_STATE_UNINITIALIZED : 0,
+		READY_STATE_LOADING : 1,
+		READY_STATE_LOADED : 2,
+		READY_STATE_INTERACTIVE : 3,
+		READY_STATE_COMPLETE : 4 //si ha terminado de recibir los datos
+	}
+};
+
+
 //validacion de formulario en cliente
-var inti = function(e_load){
+var init = function(e_load){
 	document.querySelector('[type:"submit"]').addEvenListener('click', validaciones, false);
 	var attrCliente = {
 		name: 'Pepito',
 		surname: 'de los Palotes'
 	};
+
+	//validacion cliente
 	var cliente = new Cliente(attrCliente);
 	if ( cliente.getName() !== '' )
 		document.documentElement.lastChild.innerHTML += '<strong>'.cliente.getName().'</strong>';
 
+	//cargar datos customer
+	AJAX.xhr = AJAX.shim(); 
+	if( AJAX.xhr != null ){
+		document.querySelector("#loadDataCustomer").addEventListener("click", function(){
+			var ajaxParams = {
+				url: '/templ/customer',
+				type: 'POST',
+				dataType: 'text',
+				data: null
+			};
+			ajaxRequest(ajaxParams.url, ajaxParams.type, ajaxParams.dataType, ajaxParams.data);
+		}, false);
+	} else
+		console.log("Este navegador no soporta peticiones asíncronas");
 }
+
+//recuperar petioc ajax
+var ajaxRequest = function(paramsRequest){
+	//controlar peticion ajax
+};
+
 //prototypo de cliente
 var Cliente = function(attrClient){
 	var attrClient = attrClient || {};
--- validacion.php
+++ validacion.php
@@ -1,1 +1,27 @@
-validacion de formulario en servidor
+<?php
+/* =======================================================
+   CONEXION CON PDO
+   ======================================================= */
+try {
+	$conn = new PDO('mysql:host=localhost;dbname=myDataBase', 'user', 'pass');
+	$conn->query('SET NAMES \'utf8\'');
+	$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION); //fijar errores de PDOException
+} catch (PDOException $e) {
+	echo "Error de conexion a BBDDD nº:".$e->getCode()."<br/>".$e->getMessage()."";
+	exit();
+}
+
+$resultFamilia = $conn->query("SELECT cod, nombre FROM familia");
+if($resultFamilia) {
+	while ($row = $resultFamilia->fetch(PDO::FETCH_OBJ)) {
+		$arrFamilia[] = $row->nombre; //campo o atributo de la tupla
+	}
+	return $arrFamilia;
+}
+
+$resultFamilia = null;//cerramos buffer de peticion
+$conn = null;//cerramos conexion a Mysql
+?>
+
+
+<?php
